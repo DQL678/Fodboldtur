c00848826d2b1a7d05e3fd4546c708691234bc88
DQL678 <dungql678@gmail.com>
2025-11-06 15:21:07 +0100

    GUI

---
+++ GUI.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import ttk
+
+window = Tk()
+window.geometry("500x500")
+window.title("GUI fodboldtur")
+
+icon = PhotoImage(file='fodbold.png')
+window.iconphoto(True, icon)
+
+window.mainloop()
